85bba78b01553de6fdeee025c06a4f209cccb480
sanskriti932 <lamichhanesans@gmail.com>
2024-11-22 13:58:37 +0545

    neww

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'], // Adjust paths as needed
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+    },
 });
